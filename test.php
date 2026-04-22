f41727aacd48dd516f71e171c139070e599008d9
fatimah-suhayla <aldifatimahsa@gmail.com>
2026-04-22 15:27:53 +0700

    Create test.php

---
+++ test.php
@@ -0,0 +1,14 @@
+<section>
+        <?php
+            $teks - "SELAMAT SIANG" ;
+            $_a =10;
+            $b = true;
+            $c = 9 . 45 ;
+
+        echo "$teks";
+        echo "$_a";
+        echo "$b";
+        echo "$c";
+        ?>
+    </section>
+    
